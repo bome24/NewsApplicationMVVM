42850c84341aa51c7daa192db38e9131f946754c
bome24 <81296575+bome24@users.noreply.github.com>
2023-03-19 14:58:44 +0900

    FEAT: Set TableView Cells

---
+++ GoodNewsMVVM/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNewsMVVM
+//
+//  Created by BoMin on 2023/03/19.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
--- GoodNewsMVVM/Controllers/NewsTableViewController.swift
+++ GoodNewsMVVM/Controllers/NewsTableViewController.swift
@@ -11,11 +11,14 @@ import RxSwift
 class NewsTableViewController: UITableViewController {
     
     let disposeBag = DisposeBag()
+    private var articleListVM: ArticleListViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.view.backgroundColor = UIColor(displayP3Red: 87 / 255, green: 101 / 255, blue: 116 / 255, alpha: 1.0)
+        self.tableView.backgroundColor = UIColor(displayP3Red: 87 / 255, green: 101 / 255, blue: 116 / 255, alpha: 1.0)
         
         populateNews()
     }
@@ -25,8 +28,16 @@ class NewsTableViewController: UITableViewController {
         let resource = Resource<ArticlesList>(url: URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=b85fd2ea4fda4570a2cd9e191a05f021")!)
         
         URLRequest.load(resource: resource)
-            .subscribe(onNext: {
-                print($0)
+            .subscribe(onNext: { articleResponse in
+                
+                let articles = articleResponse.articles
+                
+                self.articleListVM = ArticleListViewModel(articles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
             }).disposed(by: disposeBag)
         
     }
@@ -35,12 +46,32 @@ class NewsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.articleListVM == nil ? 0: self.articleListVM.articlesVM.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("Article TableView Cell not found.")
+        }
+        
+        let articleVM = self.articleListVM.articleAt(indexPath.row)
+        
+        articleVM.title.asDriver(onErrorJustReturn: "")
+            .drive(cell.titleLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        articleVM.description.asDriver(onErrorJustReturn: "")
+            .drive(cell.descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        cell.backgroundColor = UIColor(displayP3Red: 87 / 255, green: 101 / 255, blue: 116 / 255, alpha: 1.0)
+        
+        return cell
     }
 
 }
